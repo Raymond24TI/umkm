d10d4e5e19c8500791ecd58f4e08675fd7c54f63
raymoond24ti <raymond24ti@mahasiswa.pcr.ac.id>
2025-09-22 19:17:17 +0700

    test

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,10 +1,25 @@
 <?php
+    namespace App;
 
-namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
 
-use Illuminate\Database\Eloquent\Model;
+    class Order extends Model
+    {
+        use SoftDeletes;
 
-class Order extends Model
-{
-    //
-}
+        protected $fillable = [
+            'product_id', 'user_id', 'quantity', 'address'
+        ];
+
+        public function user()
+        {
+            return $this->belongsTo(User::class, 'user_id');
+        }
+
+        public function product()
+        {
+            return $this->belongsTo(Product::class, 'product_id');
+        }
+
+    }
